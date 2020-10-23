dbc87f70a8aaa8df92e35c8b2e0cd2ccda65458e
wenhailin <564544234@qq.com>
2020-10-23 18:49:36 +0800

    Annotation update

--- src/main/kotlin/com/github/wenslo/fluent/adminserver/AdminServerApplication.kt
+++ src/main/kotlin/com/github/wenslo/fluent/adminserver/AdminServerApplication.kt
@@ -1,9 +1,13 @@
 package com.github.wenslo.fluent.adminserver
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
 
 @SpringBootApplication
+@Configuration
+@EnableAdminServer
 class AdminServerApplication
 
 fun main(args: Array<String>) {
